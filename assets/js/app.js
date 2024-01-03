a9f9eef75085d117447b9295893f0d1421d2e0ba
nikhil-250 <you@example.com>
2024-01-03 23:59:07 +0530

    work on navbar

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+// const cl = console.log;
+
+// let togglebutn = document.getElementById(`togglebutn`);
+
+// const onclickToggle = ele => {
+//   cl(ele.target)
+//   ele.target.classList.toggle(`fa-xmark`)
+//   ele.target.classList.toggle(`fa-bars`)
+// }
+
+// togglebutn.addEventListener(`click`,onclickToggle)
